6bcebf08a496452da996309ad678211ef8ce29cd
Youmna-Younis <83347951+Youmna-Younis@users.noreply.github.com>
2024-07-28 22:06:00 +0300

    Delete app/app directory

--- app/app/app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
-
-class UploadFileForm(FlaskForm):
-    file = FileField("File")
-    submit = SubmitField("Upload File")
